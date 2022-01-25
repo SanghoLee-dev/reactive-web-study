62e781c8d04042863514a2c0c48ce1734cac0683
sangho lee <sangholee.dev@gmail.com>
2022-01-25 20:35:42 +0900

    feat: member 라우터 bean 으로 추가

--- src/main/java/com/github/sangholee_dev/reactivewebstudy/member/api/MemberRouter.java
+++ src/main/java/com/github/sangholee_dev/reactivewebstudy/member/api/MemberRouter.java
@@ -2,6 +2,7 @@ package com.github.sangholee_dev.reactivewebstudy.member.api;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -11,8 +12,10 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 @Configuration(proxyBeanMethods = false)
 public class MemberRouter {
 
-	public RouterFunction<ServerResponse> route(final MemberHandler memberHandler) {
-		return RouterFunctions.route(GET("/members/{id}").and(accept(MediaType.APPLICATION_JSON)),
-			memberHandler::getMemberById);
+	@Bean
+	public RouterFunction<ServerResponse> memberRoute(final MemberHandler memberHandler) {
+		return RouterFunctions.route().GET("/members/{id}",
+			accept(MediaType.APPLICATION_JSON),
+			memberHandler::getMemberById).build();
 	}
 }
